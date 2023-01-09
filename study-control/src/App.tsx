57ed4527d6f2959d1ec21eb9c6d7a57701d95e15
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2023-01-09 15:38:17 -0300

    feat: Crindo o componente Button e renderizando no app

--- study-control/src/App.tsx
+++ study-control/src/App.tsx
@@ -1,19 +1,9 @@
+import Button from "./components/Button/button";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello, world! React JS
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button />
     </div>
   );
 }
